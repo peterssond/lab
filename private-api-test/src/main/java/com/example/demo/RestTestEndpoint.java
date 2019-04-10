68b7fdfe5a8e49391f5e041a1db18ecb42a0026d
Daniel Petersson <daniel.pete@gmail.com>
2019-04-10 15:03:39 +0200

    TEST-3: Added Bullar

--- private-api-test/src/main/java/com/example/demo/RestTestEndpoint.java
+++ private-api-test/src/main/java/com/example/demo/RestTestEndpoint.java
@@ -28,4 +28,9 @@ public class RestTestEndpoint {
     public String test3() {
         return "REPONSE FROM TEST2 REST ENDPOINT (ADMIN), MOOOORE";
     }
+
+    @GetMapping(path = "/test4")
+    public String test4() {
+        return "BULLE";
+    }
 }
